67f8f4f9e8f88167ea937340e18560c8c3b6edf3
Brad West <brad@bradonomics.com>
2021-01-23 10:24:15 -0600

    Change mail server to Zoho

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,13 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'lit-lake-04141.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'portholes.app' }
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'lit-lake-04141.herokuapp.com',
+    :port           => ENV['ZOHO_SMTP_PORT'],
+    :address        => ENV['ZOHO_SMTP_SERVER'],
+    :user_name      => ENV['ZOHO_SMTP_LOGIN'],
+    :password       => ENV['ZOHO_SMTP_PASSWORD'],
+    :domain         => 'portholes.app',
     :authentication => :plain,
   }
 
