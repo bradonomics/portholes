4a2809dd767b87cb7c5631b67d2aa91fd82f39a8
Brad West <brad@bradonomics.com>
2021-01-23 09:05:15 -0600

    Add redirect for /user path

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,18 @@ Rails.application.routes.draw do
   # root "folders#show"
   root to: redirect('/folder/unread')
 
-  devise_for :user
-  # controllers: {
-  #   sessions: 'users/sessions'
-  # },
+  devise_for :user,
+  controllers: {
+    registrations: 'users/registrations'
+  }
   # path: "",
   # path_names: {
   #   sign_in: "login",
   #   sign_out: "logout",
   #   sign_up: "register"
   # }
+
+  get '/user', to: redirect('/folder/unread')
 
   get "folder/:permalink" => "folders#show", as: "folder_permalink"
   resources :folders, path: "/folder" do
