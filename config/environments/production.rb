8f0793c152fc64e02f73d5dff0fa445c9be3c5f8
Brad West <brad@bradonomics.com>
2021-01-23 09:32:41 -0600

    Change mailgun host in an attempt to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,13 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'portholes.app' }
+  config.action_mailer.default_url_options = { host: 'lit-lake-04141.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'portholes.app',
+    :domain         => 'lit-lake-04141.herokuapp.com',
     :authentication => :plain,
   }
 
