1a4490f3d0c009b9e3bc89522753749e2b943578
Brad West <brad@bradonomics.com>
2021-02-21 11:26:02 -0600

    Send document to ArticleParser if Readability fails

--- app/services/article_writer.rb
+++ app/services/article_writer.rb
@@ -2,7 +2,7 @@ module ArticleWriter
   def self.write(target_file, article, title, host)
     File.open(target_file, 'w') do |outfile|
       outfile.puts "<html><head><title>#{title}</title></head>"
-      outfile.puts "<h2>#{title}</h2><p>#{host}</p><hr>"
+      outfile.puts "<body><h2>#{title}</h2><p>#{host}</p><hr>"
       outfile.puts article
       outfile.puts "</body></html>"
     end
--- app/services/download.rb
+++ app/services/download.rb
@@ -26,8 +26,11 @@ class Download
       # Send document to Readability for parsing
       article, article_status = Open3.capture2("node lib/readability/content.js", stdin_data: document)
 
-      # If Readability fails, start on the next loop
-      next unless article_status == 0
+      # If Readability fails, use home-built parser
+      # next unless article_status == 0
+      document.to_html(:encoding => 'UTF-8')
+      document.to_s
+      article = ArticleParser.parse(document) unless article_status == 0
 
       # Add title and file_name to files array
       title = url.title
