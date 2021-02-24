57a2c81d5ad6ea96bc65ef3242cabd58bac12de5
Brad West <brad@bradonomics.com>
2021-02-24 08:51:53 -0600

    Update navigation

--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -9,11 +9,24 @@ document.addEventListener('click', function(event) {
   // Sub-menu toggler (desktop)
   if (event.target == document.querySelector('.sub-menu-toggle')) {
     event.preventDefault();
-    subMenuToggler(document.querySelector('.sub-menu-toggle'));
+    subMenuToggler();
   }
 
-});
+  // Clicking off the menu closes menu
+  if (window.innerWidth >= 938) {
+    if (event.target != document.querySelector('.sub-menu-toggle') && event.target != document.querySelector('.sub-menu')) {
+      closeNavigation();
+    }
+  }
 
+  // Pressing Esc closes menu
+  document.onkeydown = function(event) {
+    if (event.keyCode == 27) {
+      closeNavigation();
+    }
+  };
+
+});
 
 window.mobileNavToggler = function() {
   // Main nav toggler for mobile devices
@@ -60,39 +73,61 @@ window.mobileNavToggler = function() {
   }
 };
 
-window.subMenuToggler = function(toggler) {
+window.subMenuToggler = function() {
   // Sub-menu toggler (desktop)
-  console.log('I heard a click.');
 
-  // In this repo the toggler chevron is before the `a` link.
-  // It requires the chaining of `nextElementSibling` to get the sub-menu
+  // Get the sub-menu
   var subNavigation = document.querySelector('.sub-menu');
+  // Get the sub-menu-toggler
+  var toggler = document.querySelector('.sub-menu-toggle');
 
-  if (subNavigation.getAttribute('data-collapsed') === 'true') {
-    // Add class expanded to .sub-menu
-    subNavigation.className += ' expanded';
-
-    // Add class expanded to .sub-menu-toggle
-    toggler.className += ' expanded';
-
-    // mark Aria toggle button expanded as true
-    toggler.setAttribute('aria-expanded', 'true');
-
-    // set data-collapsed to false
-    subNavigation.setAttribute('data-collapsed', 'false');
-
-  } else {
-    // Remove expanded class from .sub-menu
-    subNavigation.classList.remove('expanded');
-
-    // Remove expanded class from .sub-menu-toggler
-    toggler.classList.remove('expanded');
-
-    // mark the section as collapsed
-    subNavigation.setAttribute('data-collapsed', 'true');
-
-    // mark Aria toggle button expanded as false
-    toggler.setAttribute('aria-expanded', 'false');
+  if (subNavigation.getAttribute('data-collapsed') == 'true') {
+    openNavigation();
+    return false;
   }
 
+  if (subNavigation.getAttribute('data-collapsed') == 'false') {
+    closeNavigation();
+    return false;
+  }
+
+};
+
+window.openNavigation = function() {
+  // Get the sub-menu
+  var subNavigation = document.querySelector('.sub-menu');
+  // Get the sub-menu-toggler
+  var toggler = document.querySelector('.sub-menu-toggle');
+
+
+  // Add class `expanded` to .sub-menu
+  subNavigation.className += ' expanded';
+
+  // Add class `expanded` to .sub-menu-toggle
+  toggler.className += ' expanded';
+
+  // mark Aria toggle button expanded as true
+  toggler.setAttribute('aria-expanded', 'true');
+
+  // set data-collapsed to false
+  subNavigation.setAttribute('data-collapsed', 'false');
+};
+
+window.closeNavigation = function() {
+  // Get the sub-menu
+  var subNavigation = document.querySelector('.sub-menu');
+  // Get the sub-menu-toggler
+  var toggler = document.querySelector('.sub-menu-toggle');
+
+  // Remove expanded class from .sub-menu
+  subNavigation.classList.remove('expanded');
+
+  // Remove expanded class from .sub-menu-toggler
+  toggler.classList.remove('expanded');
+
+  // mark the section as collapsed
+  subNavigation.setAttribute('data-collapsed', 'true');
+
+  // mark Aria toggle button expanded as false
+  toggler.setAttribute('aria-expanded', 'false');
 };
