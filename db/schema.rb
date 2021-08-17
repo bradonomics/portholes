2459399beab24c3604616939ff0df1b745ef5974
Brad West <brad@bradonomics.com>
2021-08-17 17:02:42 -0500

    Add body column to Article ActiveRecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_123305) do
+ActiveRecord::Schema.define(version: 2021_08_16_190950) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_123305) do
     t.bigint "folder_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "body"
     t.index ["folder_id"], name: "index_articles_on_folder_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
