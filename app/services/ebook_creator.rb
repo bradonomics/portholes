0f3063e6e9c4b730a46467e388264ea23524bbf6
Brad West <brad@bradonomics.com>
2021-12-23 12:34:23 -0600

    Add folder name as author name for better display on Kindle

--- app/services/ebook_creator.rb
+++ app/services/ebook_creator.rb
@@ -1,16 +1,18 @@
 require 'fileutils'
 
 module EbookCreator
-  def self.mobi(user_directory, full_directory_path, ebook_file_name)
+  def self.mobi(user_directory, full_directory_path, ebook_file_name, folder)
+    author = folder.name
     system("zip -r #{full_directory_path}.zip #{full_directory_path}")
-    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.mobi --authors \"Portholes\" --no-inline-toc --chapter 'page' --change-justification 'left' --page-breaks-before '/'")
+    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.mobi --authors \"#{author}\" --no-inline-toc --chapter 'page' --change-justification 'left' --page-breaks-before '/'")
     File.delete("#{full_directory_path}.zip")
     FileUtils.rm_r(full_directory_path)
   end
 
-  def self.epub(user_directory, full_directory_path, ebook_file_name)
+  def self.epub(user_directory, full_directory_path, ebook_file_name, folder)
+    author = folder.name
     system("zip -r #{full_directory_path}.zip #{full_directory_path}")
-    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.epub --authors \"Portholes\" --chapter 'page' --change-justification 'left' --page-breaks-before '/'")
+    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.epub --authors \"#{author}\" --chapter 'page' --change-justification 'left' --page-breaks-before '/'")
     File.delete("#{full_directory_path}.zip")
     FileUtils.rm_r(full_directory_path)
   end
