926e58fa48377c6674e8c286a60d65b97c07857e
Brad West <brad@bradonomics.com>
2022-02-10 16:55:52 -0600

    Change the title display on reading device

--- app/services/ebook_creator.rb
+++ app/services/ebook_creator.rb
@@ -12,15 +12,17 @@ module EbookCreator
       book_format = 'azw3'
     end
 
+    date = Date.today
     system("zip -r #{full_directory_path}.zip #{full_directory_path}")
-    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.#{book_format} --authors \"#{folder.name}\" --no-inline-toc --chapter 'page' --change-justification 'left' --page-breaks-before '/' #{mobi_file_type}")
+    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.#{book_format} --authors \"Portholes\" --no-inline-toc --chapter 'page' --title \"#{folder.name}: #{date.strftime('%a, %d %b %Y')}\" --change-justification 'left' --page-breaks-before '/' #{mobi_file_type}")
     File.delete("#{full_directory_path}.zip")
     FileUtils.rm_r(full_directory_path)
   end
 
   def self.epub(current_user, user_directory, full_directory_path, ebook_file_name, folder)
+    date = Date.today
     system("zip -r #{full_directory_path}.zip #{full_directory_path}")
-    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.epub --authors \"#{folder.name}\" --chapter 'page' --change-justification 'left' --page-breaks-before '/'")
+    system("ebook-convert #{full_directory_path}.zip #{user_directory}/#{ebook_file_name}.epub --authors \"Portholes\" --chapter 'page' --title \"#{folder.name}: #{date.strftime('%a, %d %b %Y')}\" --change-justification 'left' --page-breaks-before '/'")
     File.delete("#{full_directory_path}.zip")
     FileUtils.rm_r(full_directory_path)
   end
